219728d86eef0344f751ac73dcb322166cc0787c
jaro <roskot.j@proton.me>
2025-12-16 13:16:05 +0100

    Add someting

---
+++ eqSelection.js
@@ -0,0 +1,7 @@
+function openLinea() {
+    document.getElementById("linea").style.zIndex = "2";
+}
+
+function openKvadr() {
+    document.getElementById("kvadr").style.zIndex = "2";
+}
---
+++ linea.js
@@ -0,0 +1,5 @@
+function lineaMath() {
+    var a = document.getElementsByClassName("A").value;
+    var b = document.getElementsByClassName("B").value;
+    var c = document.getElementById("C").value;
+}
