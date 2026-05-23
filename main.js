79d41ad4bd9ca760fd38707bf4cffffac4ff45d9
jaro <roskot.j@proton.me>
2026-05-23 16:08:13 +0200

    redesign site and add support for answering in complex numbers

--- main.js
+++ main.js
@@ -1,56 +1,42 @@
-function Math() {
-    let a = document.getElementById("kA").value;
-    let b = document.getElementById("kB").value;
-    let c = document.getElementById("C").value;
+function calc() {
+  let a = document.getElementById("kA").value;
+  let b = document.getElementById("kB").value;
+  let c = document.getElementById("C").value;
+  let ans = document.getElementById("ans");
+  let menu = document.getElementById("menu");
 
-    if(a == 0) {
-      if(b == 0) {
-          if(c == 0) {
-              document.getElementById("ans").innerText = "the equation has infinit number of solutions";
-          }
-          else {
-              document.getElementById("ans").innerHTML = "the equation has no solution";
-          }
+  if(a == 0) {
+    if(b == 0) {
+      if(c == 0) {
+        ans.innerText = "you have not entered the equasion";
       }
       else {
-          let x = (-c)/b
-          console.log(x)
-          document.getElementById("ans").innerHTML = "the answer is: " + "x = " + x;
+        ans.innerHTML = "the equation has no solution";
       }
     }
     else {
-        let D = b**2 - 4*a*c
-        let x1 = (-b + D**0.5)/(2*a)
-        let x2 = (-b - D**0.5)/(2*a)
-        if(D < 0) {
-            document.getElementById("ans").innerHTML = "the equation has no solution"
-        }
-        else {
-            document.getElementById("ans").innerHTML = "the answers are: " + "x1 = " + x1 + ", " + "x2 = " + x2;
-        }
+      let x = (-c)/b
+      ans.innerHTML = "the answer is: " + "x = " + x;
     }
-    box.style.zIndex = 2;
-    box.style.opacity = "100%";
-    box.style.borderColor = "rgb(255, 150, 150)";
-}
-function lineaMath() {
-
-    if(b == 0) {
-        if(c == 0) {
-            document.getElementById("ans").innerText = "the equation has infinit number of solutions";
-        }
-        else {
-            document.getElementById("ans").innerHTML = "the equation has no solution";
-        }
+  }
+  else {
+    D = b**2 - 4*a*c
+    x1 = (-b + D**0.5)/(2*a)
+    x2 = (-b - D**0.5)/(2*a)
+    if (D < 0) {
+      x = -b / (2 * a)
+      m = ""
+      if (x < 0) {
+        x = x * -1
+        m = "- "
+      }
+      xi = Math.round(((-1 * D) ** 0.5 * 1000) / (2 * a)) / 1000 + "i"
+      ans.innerHTML = "the answers are: " + "x1 = " + m + x + " + " + xi + ", " + "x2 = " + m + x + " - " + xi;
     }
     else {
-        console.log(a)
-        console.log(b)
-        let x = (-b)/a
-        console.log(x)
-        document.getElementById("ans").innerHTML = "the answer is: " + "x = " + x;
+      ans.innerHTML = "the answers are: " + "x1 = " + x1 + ", " + "x2 = " + x2;
     }
-    box.style.zIndex = 2;
-    box.style.opacity = "100%";
-    box.style.borderColor = "rgb(150, 150, 255)";
+  }
+  ans.style.pointerEvents = "all";
+  ans.style.opacity = 1;
 }
